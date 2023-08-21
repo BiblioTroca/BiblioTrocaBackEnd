21ba1a332c2958f47550c0ad2a0aff45a8ac4743
Pessinaaa <pedro.pessina99@gmail.com>
2023-08-21 09:39:42 -0300

    :seedling: Initial commit

---
+++ BiblioTroca/src/main/java/com/BiblioTroca/BiblioTroca/BiblioTrocaApplication.java
@@ -0,0 +1,13 @@
+package com.BiblioTroca.BiblioTroca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioTrocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioTrocaApplication.class, args);
+	}
+
+}
---
+++ BiblioTroca/src/test/java/com/BiblioTroca/BiblioTroca/BiblioTrocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BiblioTroca.BiblioTroca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioTrocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
