11db2e2fff782e77825a4ff483782604d3277884
Pessinaaa <pedro.pessina99@gmail.com>
2023-08-25 10:56:34 -0300

    :construction: CRUD Usuário - finalizado, revisar imports

---
+++ BiblioTroca/src/main/java/dto/UserDTO.java
@@ -0,0 +1,64 @@
+package dto;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+public class UserDTO {
+	private String name;
+	private String surname;
+	private String email;
+	private String telephone;
+	private String cep;
+	
+	public UserDTO() {	}
+	
+	public UserDTO(String newName, String newSurname, String newEmail, String newTelephone, String newCep) {
+		this.name = newName;
+		this.surname = newSurname;
+		this.email = newEmail;
+		this.telephone = newTelephone;
+		this.cep = newCep;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	public static UserDTO returnUserDTO(User user) {
+		return new UserDTO(user.getName(), user.getSurname(), user.getEmail(), user.getTelephone(), user.getCep());
+	}
+	
+	public static User returnUser(UserDTO, userDTO) {
+		return new User(userDTO.getName(), userDTO.getSurname(), userDTO.getEmail(), userDTO.getTelephone(), userDTO.getCep());
+	}
+}
--- BiblioTroca/src/main/java/service/UserService.java
+++ BiblioTroca/src/main/java/service/UserService.java
@@ -1,8 +1,65 @@
 package service;
 
 import org.springframework.stereotype.Service;
+import controller.Autowired;
 
 @Service
 public class UserService {
-
+	private UserRepository userRepository;
+	
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	public User createNewUser(User user) throws CpfAlreadyInUseException {
+		if(this.userRepository.existsByCpf(user.getCpf())) {
+			throw new CpfAlreadyInUseException(user.getCpf());
+		}
+		return this.userRepository.save(user);
+	}
+	
+	public List<User> returnAllUsers() {
+		return this.userRepository.findAll();
+	}
+	
+	public User returnUserByCPF(String cpf) throws CpfNotFoundException {
+		if(this.userRepository.existsByCpf(cpf)) {
+			return this.userRepository.findByCpf(cpf);
+		}
+		throw new CpfNotFoundException(cpf);
+	}
+	
+	public User updateUser(String cpf, User user) throws CpfNotFoundException {
+		if(!this.userRepository.findByCpf(cpf)) {
+			throw new CpfNotFoundException(cpf);
+		}
+		User userRequest = this.userRepository.getReferenceByCpf(cpf);
+		user.setId(userRequest.getId());
+		user.setCpf(cpf);
+		if(user.getName() == null) {
+			user.setName(userRequest.getName());
+		}
+		if(user.getSurname() == null) {
+			user.setSurame(userRequest.getSurame());
+		}
+		if(user.getEmail() == null) {
+			user.setEmail(userRequest.getEmail());
+		}
+		if(user.getTelephone() == null) {
+			user.setTelephone(userRequest.getTelephone());
+		}
+		if(user.getCep() == null) {
+			user.setCep(userRequest.getCep());
+		}
+		return this.userRepository.save(user);
+	}
+	
+	public void deleteUser(String cpf) throws CpfNotFoundException {
+		if(this.userRepository.findByCpf(cpf)) {
+			this.userRepository.deleteByCpf(cpf);
+		} else {
+			throw new CpfNotFoundException(cpf);
+		}
+	}
 }
