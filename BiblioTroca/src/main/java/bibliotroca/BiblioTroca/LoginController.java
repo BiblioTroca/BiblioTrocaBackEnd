b4a04a20c4d12a0f2571bdb788cb48e357527a13
guilhermegg8 <guiguigoncalvess@gmail.com>
2023-10-16 15:42:57 -0300

    :sparkles: Inserindo sistema de login com OAuth

---
+++ BiblioTroca/src/main/java/bibliotroca/BiblioTroca/LoginController.java
@@ -0,0 +1,34 @@
+package bibliotroca.BiblioTroca;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String getHomePage(Authentication authentication, Model model){
+
+        DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+        Map<String, Object> attributes = user.getAttributes();
+
+        String name = attributes.getOrDefault("localizedFirstName", attributes.get("given_name")).toString();
+        String email = attributes.get("email").toString();
+
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login";
+    }
+
+}
